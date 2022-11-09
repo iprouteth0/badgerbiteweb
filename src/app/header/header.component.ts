2cccf8a4ea9534b19004f792c72a7ef2f542dd29
Thor Validator <t.demis@gmail.com>
2022-11-09 20:18:10 +0200

    Ticker

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { LeftHandMenuService } from "../service/left-hand-menu.service";
 import { StateService } from "../service/state.service";
 import { CHAINS } from '../data/data';
+import { HttpClient } from '@angular/common/http';
+import { ChainService } from "../service/chain.service";
+import { Chain } from "../model/chain";
 
 @Component({
   selector: 'app-header',
@@ -9,13 +12,38 @@ import { CHAINS } from '../data/data';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  chain: String[] = [];
+  ChainId: String[] = [];
+  Price: String[] = [];
+  Change: String[] = [];
+  Negative: Boolean[] = [];
+  chainSummarySubscription: any;
 
 sliderTopbar?: boolean;
 
-
   constructor(private leftHandMenuService: LeftHandMenuService,
-              public stateService: StateService) {
+              public stateService: StateService,private http: HttpClient, public chainService: ChainService) {
+
+    for (let i = 0; i < CHAINS.length; i++) {
+      let apiChainId = CHAINS[i].apiChainId || CHAINS[i].id;
+      this.chain[i]=apiChainId
+      let coingekoCoinId = CHAINS[i].coingekoCoinId || CHAINS[i].id;
+      this.ChainId[i]=apiChainId
+      this.chainSummarySubscription = this.chainService.getCoingekoSummary(coingekoCoinId)
+      .subscribe((coingekoSummary: any) => {
+        this.Price[i] = this.extractPrice(coingekoSummary);
+        this.Change[i] = this.extractPriceChange(coingekoSummary);
+        this.Negative[i]=true
+        let a = Number(this.Change[i]) || 0;
+        if (a <= 0) { 
+          this.Negative[i]=false
+        }
+        this.Change[i]=a.toFixed(2) +'%'
+      });
+
+
+                }            
+
   }
 
   ngOnInit(): void {
@@ -23,5 +51,35 @@ sliderTopbar?: boolean;
 
   openLeftHandMenuForMobile(): void {
     this.leftHandMenuService.openLeftHandMenuForMobile();
+  }
+
+  extractPrice(coingekoSummary: any): string {
+    let price = coingekoSummary?.market_data?.current_price?.usd;
+    if (!price) {
+      return '-';
+    }
+    return Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumSignificantDigits: 4
+    }).format(price);
+  }
+  
+  extractPriceUnformated(coingekoSummary: any): any {
+    let price = coingekoSummary?.market_data?.current_price?.usd;
+    if (!price) {
+      return '-';
+    }
+
+    return price
+  }
+
+  extractPriceChange(coingekoSummary: any): any {
+    let price = coingekoSummary?.market_data?.price_change_percentage_24h;
+    if (!price) {
+      return '-';
+    }
+
+    return price
   }
 }
