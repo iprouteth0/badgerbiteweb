a68a2e9f3f4f24645d7e5c8b7edc0fb732720fc6
Thor Validator <t.demis@gmail.com>
2022-11-12 18:26:38 +0200

    Header Links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,6 +19,7 @@ export class HeaderComponent implements OnInit {
   @Input() buttonList: boolean=true;
 
   chain: String[] = [];
+  testnet: String[] = [];
   ChainId: String[] = [];
   Price: String[] = [];
   Change: String[] = [];
@@ -99,7 +100,79 @@ export class HeaderComponent implements OnInit {
     return price
   }
 
+  _activateMenuDropdown() {
+    /**
+     * Menu activation reset
+     */
+    const resetParent = (el) => {
+      el.classList.remove("active");
+      const parent = el.parentElement;
 
+      /**
+       * TODO: This is hard coded way of expading/activating parent menu dropdown and working till level 3.
+       * We should come up with non hard coded approach
+       */
+      if (parent) {
+        parent.classList.remove("active");
+        const parent2 = parent.parentElement;
+        if (parent2) {
+          parent2.classList.remove("active");
+          const parent3 = parent2.parentElement;
+          if (parent3) {
+            parent3.classList.remove("active");
+            const parent4 = parent3.parentElement;
+            if (parent4) {
+              const parent5 = parent4.parentElement;
+              parent5.classList.remove("active");
+
+            }
+          }
+        }
+      }
+    };
+    let links = document.getElementsByClassName("nav-link-ref");
+    let matchingMenuItem = null;
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < links.length; i++) {
+      // reset menu
+      resetParent(links[i]);
+    }
+    for (let i = 0; i < links.length; i++) {
+      if (window.location.pathname === links[i]["pathname"]) {
+        // matchingMenuItem = links[i];
+        break;
+      }
+    }
+
+    if (matchingMenuItem) {
+      // matchingMenuItem.classList.add("active");
+      // const parent = matchingMenuItem.parentElement;
+
+      /**
+       * TODO: This is hard coded way of expading/activating parent menu dropdown and working till level 3.
+       * We should come up with non hard coded approach
+       */
+      // if (parent) {
+      //   // parent.classList.add("active");
+      //   // const parent2 = parent.parentElement;
+      //   if (parent2) {
+      //     parent2.classList.add("active");
+      //     const parent3 = parent2.parentElement;
+      //     if (parent3) {
+      //       parent3.classList.add("active");
+      //       const parent4 = parent3.parentElement;
+      //       if (parent4) {
+      //         const parent5 = parent4.parentElement;
+      //         parent5.classList.add("active");
+
+      //         document.getElementById("navigation").style.display = "none";
+      //         this.isCondensed = false;
+      //       }
+      //     }
+      //   }
+      // }
+    }
+  }
 
   windowScroll() {
     if (
@@ -128,6 +201,28 @@ export class HeaderComponent implements OnInit {
     } else {
       document.getElementById("navigation")!.style.display = "none";
     }
+  }
+  onMenuClick(event) {
+    event.preventDefault();
+    const nextEl = event.target.nextSibling.nextSibling;
+    if (nextEl && !nextEl.classList.contains("open")) {
+      const parentEl = event.target.parentNode;
+      if (parentEl) {
+        parentEl.classList.remove("open");
+      }
+      nextEl.classList.add("open");
+    } else if (nextEl) {
+      nextEl.classList.remove("open");
+    }
+    return false;
+  }
+
+  developerModal(content) {
+    this.modalService.open(content, { size: 'lg', centered: true });
+  }
+
+  wishListModal(content) {
+    this.modalService.open(content, { centered: true });
   }
 
 }
